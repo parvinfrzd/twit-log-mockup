3d6f20dc30dfcf785763ef8e0918078d67350bf6
Parvin Farahzadeh <parv93farah@gmail.com>
2021-12-25 21:02:24 -0500

    add new tweet ui

--- twitter-log-server/routes/tweets.js
+++ twitter-log-server/routes/tweets.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/new', function addTweet(req, res) {
+  res.render('tweets/new', { title: 'Add New Tweet' })
 });
 
 module.exports = router;
