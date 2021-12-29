413df390cd322a37eb9ece1f0335f50c96fc57b9
Parvin Farahzadeh <parv93farah@gmail.com>
2021-12-29 13:20:03 -0500

    add dependencies

--- twitter-log-server/routes/index.js
+++ twitter-log-server/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+const Users = require('../models/user');
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.redirect('/info');
+});
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// Google OAuth callback route
+router.get(
+  '/oauth2callback',
+  passport.authenticate('google', {
+    successRedirect: '/login',
+    failureRedirect: '/login',
+  })
+);
+
+// OAuth logout route
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/login');
 });
 
 module.exports = router;
