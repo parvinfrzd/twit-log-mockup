32ddf308b97c487fb570fc3b0ff84d5f8c5e43e4
Parvin Farahzadeh <parv93farah@gmail.com>
2021-12-30 16:06:15 -0500

    twitter api

--- twitter-log-server/routes/search.js
+++ twitter-log-server/routes/search.js
@@ -1,16 +1,36 @@
 var express = require('express');
 var router = express.Router();
 const Users = require('../models/user');
+const Twit = require('twit');
+
+var client = new Twit({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
 
 /* GET home page. */
-router.get('/search', function (req, res, next) {
-    res.render('search/index',
-        {
-            Users,
-            user: req.user,
-            name: req.query.name,
-            title: 'Search Tweet Feels',
-        });
+router.get('/search', async (req, res, next) => {
+
+    await client.get('search/tweets', { q: '#vegan since:2020-04-15', count: 4 }, function (err, data, response) {
+        const tweets = data.statuses
+            .map(tweet => tweet.text)
+        console.log(tweets);
+        res.render('search/index',
+            {
+                Users,
+                user: req.user,
+                name: req.query.name,
+                title: 'Search Tweet Feels',
+                tweets,
+            });
+    });
+    // console.log(tweet_results)
+
+
+
 });
 
 module.exports = router;
