97fff4734d22260e45269834d83895a3a9fc9a61
Parvin Farahzadeh <parv93farah@gmail.com>
2022-01-10 18:27:15 -0500

    add category controller

---
+++ twitter-log-server/controllers/categories.js
@@ -0,0 +1,49 @@
+const Tweet = require('../models/tweet');
+const Category = require('../models/category');
+const Users = require('../models/user');
+
+
+function addToCategory(req, res) {
+    Tweet.findById(req.params.id, function (err, tweet) {
+        tweet.category.push(req.body.categoryId);
+        tweet.save(function (err) {
+            console.log(err);
+            res.redirect(`/tweets/${tweet._id}`);
+        });
+    });
+}
+
+function createCategory(req, res) {
+    console.log('category created');
+    Category.create(req.body, function (err, category) {
+        res.redirect('/category/new');
+    });
+}
+
+function newCategory(req, res) {
+    Category.find({}, function (err, categs) {
+        res.render('category/new', {
+            title: 'Add Category',
+            categs
+        });
+    })
+}
+
+function index(req, res) {
+    Category.find({}, function (err, categs) {
+        res.render('category/index', {
+            title: 'All Categories',
+            categs,
+            Users,
+            user: req.user,
+            name: req.query.name,
+        })
+    })
+}
+
+module.exports = {
+    newCategory,
+    createCategory,
+    addToCategory,
+    index
+};
