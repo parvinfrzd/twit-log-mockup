28699861d456a18ec40a66ac9f6f04eaa1bf1b23
Parvin Farahzadeh <parv93farah@gmail.com>
2022-01-11 22:48:20 -0500

    add show tweets route

--- twitter-log-server/routes/categories.js
+++ twitter-log-server/routes/categories.js
@@ -5,5 +5,8 @@ const CategoryCtrl = require('../controllers/categories');
 router.get('/category', CategoryCtrl.newCategory);
 router.post('/category', CategoryCtrl.createCategory);
 router.post('/tweets/:id/category', CategoryCtrl.addToCategory);
+router.get('/category/:id', CategoryCtrl.showTweets);
+
 
 module.exports = router;
+
